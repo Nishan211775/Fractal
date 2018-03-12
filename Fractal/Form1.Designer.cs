f5b09acd0e7c59ed525ddda107d4910b0920d374
Nishan Dhungana <nishandhungana41@gmail.com>
2018-03-12 20:58:56 +0545

    picture loaded

---
+++ Fractal/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Fractal
+{
+    partial class Fractal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cloneToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.tagToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.characterEncodingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem,
+            this.reloadToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.startToolStripMenuItem,
+            this.cloneToolStripMenuItem,
+            this.tagToolStripMenuItem,
+            this.infoToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.characterEncodingToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.printToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.propertiesToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.closeToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            // 
+            // reloadToolStripMenuItem
+            // 
+            this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
+            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.reloadToolStripMenuItem.Text = "Reload";
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            // 
+            // cloneToolStripMenuItem
+            // 
+            this.cloneToolStripMenuItem.Name = "cloneToolStripMenuItem";
+            this.cloneToolStripMenuItem.Size = new System.Drawing.Size(175, 6);
+            // 
+            // tagToolStripMenuItem
+            // 
+            this.tagToolStripMenuItem.Name = "tagToolStripMenuItem";
+            this.tagToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.tagToolStripMenuItem.Text = "Tag";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Enabled = false;
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // characterEncodingToolStripMenuItem
+            // 
+            this.characterEncodingToolStripMenuItem.Name = "characterEncodingToolStripMenuItem";
+            this.characterEncodingToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.characterEncodingToolStripMenuItem.Text = "Character encoding";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(175, 6);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(175, 6);
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.propertiesToolStripMenuItem.Text = "Properties";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(175, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(624, 414);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Fractal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Fractal";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Fractal_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator cloneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tagToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem characterEncodingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Fractal/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Fractal : Form
+    {
+
+        private readonly int MAX = 256;      // max iterations
+        private readonly double SX = -2.025; // start value real
+        private readonly double SY = -10.125; // start value imaginary
+        private readonly double EX = 0.6;    // end value real
+        private readonly double EY = 1.125;  // end value imaginary
+        private static int x1, y1, xs, ys, xe, ye;
+        private static double xstart, ystart, xende, yende, xzoom, yzoom;
+        private static bool action, rectangle, finished;
+        private static float xy;
+        private Image picture;
+        private Graphics g;
+        
+        private Graphics g1;
+        private Cursor c1, c2;
+        private HSB HSBcol = new HSB();
+
+        public Fractal()
+        {
+            finished = false;
+            InitializeComponent();
+            x1 = Width;
+            y1 = Height;
+            xy = (float)x1 / (float)y1;
+            //this.pictureBox1.CreateGraphics();
+            c1 = Cursors.WaitCursor;
+            c2 = Cursors.Cross;
+            ///picture = pictureBox1.Image;
+            //g1 = this.pictureBox1.CreateGraphics();
+            finished = true; 
+        }
+
+        public void destroy() // delete all instances 
+        {
+            if (finished)
+            {
+                //removeMouseListener(this);
+                //removeMouseMotionListener(this);
+
+                picture = null;
+                g1 = null;
+                c1 = null;
+                c2 = null;
+                System.GC.Collect(); // garbage collection
+            }
+        }
+
+        public void start()
+        {
+            action = false;
+            rectangle = false;
+            initvalues();
+            xzoom = (xende - xstart) / (double)x1;
+            yzoom = (yende - ystart) / (double)y1;
+            mandelbrot();
+        }
+
+        public void stop()
+        {
+        }
+
+        public void paint(Graphics g)
+        {
+            update(g);
+        }
+
+        public void update(Graphics g)
+        {
+            g.DrawImage(picture, 0, 0);
+            if (rectangle)
+            {
+                //g.setColor(Color.White);
+                if (xs < xe)
+                {
+                    if (ys < ye)
+                    {
+                        g.DrawRectangle(new Pen(Color.White),xs, ys, (xe - xs), (ye - ys));
+                    }
+                    else
+                    {
+                        g.DrawRectangle(new Pen(Color.White), xs, ye, (xe - xs), (ys - ye));
+                    }
+                }
+                else
+                {
+                    if (ys < ye)
+                    {
+                        g.DrawRectangle(new Pen(Color.White), xe, ys, (xs - xe), (ye - ys));
+                    }
+                    else
+                    {
+                        g.DrawRectangle(new Pen(Color.White), xe, ye, (xs - xe), (ys - ye));
+                    }
+                }
+            }
+        }
+
+        private void mandelbrot() // calculate all points
+        {
+            int x, y = 550;
+            float h, b, alt = 0.0f;
+
+            action = false;
+            pictureBox1.Cursor = c1;
+            //showStatus("Mandelbrot-Set will be produced - please wait...");
+            for (x = 0; x < x1; x += 2)
+            {
+                for (y = 0; y < y1; y++)
+                {
+                    h = pointcolour(xstart + xzoom * (double)x, ystart + yzoom * (double)y); // color value
+                    if (h != alt)
+                    {
+                        b = 1.0f - h * h; // brightnes
+                                          ///djm added
+                                          HSBcol.fromHSB(h,0.8f,b); //convert hsb to rgb then make a Java Color
+                                          Color col = Color.FromArgb(0,(int)HSBcol.rChan, (int)HSBcol.gChan, (int)HSBcol.bChan);
+                                          //g1.setColor(col);
+                        //djm end
+                        //djm added to convert to RGB from HSB
+
+                        //HSBcol.fromHSB(h, 0.8f, b); //convert hsb to rgb then make a Java Color
+                        //Color col = new Color(0, HSBcol.rChan, HSBcol.gChan, HSBcol.bChan);
+                        
+                        //g1.SetColor(Color.getHSBColor(h, 0.8f, b));
+                        ////djm test
+                        //Color col = Color.getHSBColor(h, 0.8f, b);
+                        int red = col.R;
+                        int green = col.G;
+                        int blue = col.B;
+                        ////djm 
+                        //alt = h;
+                        
+                    }
+                    //g1.DrawLine(new Pen(Color.White), x, y, x + 1, y);
+                }
+            }
+            //showStatus("Mandelbrot-Set ready - please select zoom area with pressed mouse.");
+            //setCursor(c2);
+
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            //bmp.SetPixel(x, y, Color.Red);
+            g = Graphics.FromImage(bmp);
+
+            pictureBox1.Image = bmp;
+
+            this.pictureBox1.Cursor = c2;
+            action = true;
+        }
+
+        private float pointcolour(double xwert, double ywert) // color value from 0.0 to 1.0 by iterations
+        {
+            double r = 0.0, i = 0.0, m = 0.0;
+            int j = 0;
+
+            while ((j < MAX) && (m < 4.0))
+            {
+                j++;
+                m = r * r - i * i;
+                i = 2.0 * r * i + ywert;
+                r = m + xwert;
+            }
+            return (float)j / (float)MAX;
+        }
+
+        private void initvalues() // reset start values
+        {
+            xstart = SX;
+            ystart = SY;
+            xende = EX;
+            yende = EY;
+            if ((float)((xende - xstart) / (yende - ystart)) != xy)
+            {
+                xstart = xende - (yende - ystart) * (double)xy;
+            }
+        }
+
+        private void Fractal_Shown(object sender, EventArgs e)
+        {
+           mandelbrot();
+        }
+
+    }
+}
